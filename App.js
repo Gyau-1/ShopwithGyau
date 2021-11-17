672f71a5a3d3ca8ee54c8446146be073e740461a
Gyau-1 <91359722+Gyau-1@users.noreply.github.com>
2021-11-17 21:46:12 -0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,34 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './Screens/Home'
-import Login from './Screens/Login'
-import Product from './Screens/Product'
-import Cart from './Screens/Cart'
-import Checkout from './Screens/Checkout'
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
-import { createStackNavigator} from 'react-navigation';
+import { BookDetail } from "./screens/";
+import Tabs from "./navigation/tabs";
+import { useFonts } from 'expo-font';
 
-const RootStack = createStackNavigator( {
-  Home: Home,
-  Login: Login,
-  Product: Product,
-  Cart: Cart,
-  Checkout: Checkout,
-})
-
-
-export default class App extends React.Component {
-  render() {
-    return <RootStack/>;
-    
-}
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent"
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => {
+    const [loaded] = useFonts({
+            "Roboto-Black" : require('./assets/fonts/Roboto-Black.ttf'),
+            "Roboto-Bold" : require('./assets/fonts/Roboto-Bold.ttf'),
+            "Roboto-Regular" : require('./assets/fonts/Roboto-Regular.ttf'),
+        })
+
+    if(!loaded){
+        return null;
+    }
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Home'}
+            >
+                {/* Tabs */}
+                <Stack.Screen name="Home" component={Tabs} />
+
+                {/* Screens */}
+                <Stack.Screen name="BookDetail" component={BookDetail} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default App;
